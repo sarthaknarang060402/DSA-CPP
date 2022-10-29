c40846d80488e4dd3620e2cd5fdf2b50c6ecf1bf
Sarthak Narang <sarthaknarang666666@gmail.com>
2022-10-29 09:47:53 +0530

    committed

---
+++ Lecture-2/015-FahrenheitToCelsius.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int sf,ef,step;
+    cin>>sf>>ef>>step;
+    float cel;
+    for(float i=sf;i<ef;i+=step)
+    {
+        cel=(i-32)*5/9;
+        if(cel>0)cout<<i<<"\t"<<floor(cel)<<endl;
+        else cout<<i<<"\t"<<ceil(cel)<<endl;
+    }
+    return 0;
+}
---
+++ Lecture-2/016-Pattern1.cpp
@@ -0,0 +1,19 @@
+// Print the following pattern
+// Pattern for N = 4
+// 1
+// 23
+// 345
+// 4567
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+    return 0;
+}
