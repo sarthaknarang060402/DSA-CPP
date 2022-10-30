35552343b9d5239b73ec4e469fbdb3d823d6352a
Sarthak Narang <sarthaknarang666666@gmail.com>
2022-10-31 00:26:56 +0530

    day4

---
+++ Day4/035-Pattern18.cpp
@@ -0,0 +1,45 @@
+// Print the following pattern
+// Pattern for N = 5 and N is always odd
+//    *
+//   ***
+//  *****
+//   ***
+//    *
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<(n/2)+1;i++)
+    {
+        //spaces
+        for(int j=0;j<(n/2)-i;j++)
+        {
+            cout<<" ";
+        }
+
+        for(int j=0;j<(2*i)+1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(int i=1;i<=n/2;i++)
+    {
+        //spaces
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<n-2*i;j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
